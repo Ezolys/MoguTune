82a0f317d47833b7a0d2899541ecf50393b0d9f3
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-11-03 01:05:02 +0900

    フォルダー構成を変更

--- introqbot/client.py
+++ introqbot/client.py
@@ -2,6 +2,8 @@ import logging
 from os import getenv
 
 import discord
+import mafic
+from discord.ext import commands
 
 from introqbot.localizations import Localization
 from introqbot.logger import setup_logging
@@ -9,11 +11,34 @@ from introqbot.logger import setup_logging
 setup_logging()
 logger = logging.getLogger(__name__)
 
-intents = discord.Intents.default()
-intents.voice_states = True
-client = discord.Bot(intents=intents, debug_guilds=[1118692349250392184])
-i18n = Localization(client)
 
+class Bot(commands.Bot):
+	def __init__(self, *args, **kwargs) -> None:
+		super().__init__(*args, **kwargs)
+
+		intents = discord.Intents.default()
+		intents.voice_states = True
+
+		# Lavalink
+		self.pool = mafic.NodePool(self)
+		self.loop.create_task(self.add_nodes())
+
+	async def add_nodes(self) -> None:
+		# FIXME: 仮
+		await self.pool.create_node(
+			host="lavalinkv4.serenetia.com",
+			port=443,
+			label="AjieDev",
+			password="https://dsc.gg/ajidevserver",
+			secure=True,
+		)
+
+
+intents = discord.Intents.default()
+intents.guilds = True
+intents.voice_states = True
+client = Bot(intents=intents, debug_guilds=[1118692349250392184])
+i18n = Localization(client)
 
 # 接続完了時
 # @client.event
@@ -33,7 +58,7 @@ def run() -> None:
 	# 言語データを読み込む
 	i18n.load_locale_data()
 	# Cogs の読み込み
-	client.load_extensions("introqbot.cogs")
+	client.load_extensions("introqbot.cogs.commands")
 	# コマンドのローカライズ
 	i18n.localize_commands()
 	# Bot の起動
