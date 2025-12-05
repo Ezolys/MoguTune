c9ef80c420fc17548840a649c1d14b485d7b3dda
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-12-05 23:14:43 +0900

    実行中のセッション数を確認するコマンドを実装

--- introqbot/cogs/commands/general.py
+++ introqbot/cogs/commands/general.py
@@ -1,5 +1,7 @@
+import datetime
 import logging
 import traceback
+from os import getenv
 
 import discord
 from discord.ext import commands
@@ -9,6 +11,7 @@ from introqbot.app import App
 from introqbot.client import client
 from introqbot.debug_logger import DebugLogger
 from introqbot.embeds import EmbedsTemplates
+from introqbot.quiz_session import quiz_session_manager
 
 logger = logging.getLogger(__name__)
 
@@ -45,6 +48,35 @@ class GeneralCommands(discord.Cog):
 				inline=False,
 			)
 			await ctx.respond(embeds=[embed])
+		except Exception:
+			logger.error(traceback.format_exc())
+			await ctx.respond(
+				embed=EmbedsTemplates.internal_error(error_code=await DebugLogger.report_internal_error(traceback.format_exc()))
+			)
+
+	@commands.slash_command()
+	@discord.guild_only()
+	@discord.default_permissions(send_messages=True)
+	@commands.cooldown(2, 5)
+	async def sessions(self, ctx: discord.ApplicationContext) -> None:
+		try:
+			active_sessions = len(quiz_session_manager.sessions)
+			max_sessions = int(getenv("MAX_SESSIONS", "0"))
+			logger.info(getenv("MAX_SESSIONS"))
+
+			limit_str = t("cmd.sessions.result.limit_none")
+			if max_sessions > 0:
+				limit_str = f"{max_sessions}"
+
+			now = int(datetime.datetime.now().timestamp())
+
+			embed = EmbedsTemplates.info(
+				title=t("cmd.sessions.result.title"),
+				description=f"{t('cmd.sessions.result.last_update')}: <t:{now}:f> (<t:{now}:R>)",
+				icon="📊",
+			)
+			embed.add_field(name=t("cmd.sessions.result.current"), value=f"`{active_sessions}` / `{limit_str}`")
+			await ctx.respond(embed=embed)
 		except Exception:
 			logger.error(traceback.format_exc())
 			await ctx.respond(
