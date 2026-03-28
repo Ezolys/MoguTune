c804646a7214f10e3f274c24786f57942e28e45d
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2026-03-29 00:05:38 +0900

    yt-dlp -> youtube-plugin

--- mogutune/quiz_session.py
+++ mogutune/quiz_session.py
@@ -767,7 +767,7 @@ class QuizSession:
 		logger.info(f"Searching YouTube for: {track.author} - {track.title} (ISRC: {_isrc})")
 		try:
 			if _isrc:
-				_search_results = await self.pl.fetch_tracks(f'"{_isrc}"', mafic.SearchType.YOUTUBE)
+				_search_results = await self.pl.fetch_tracks(f'"{_isrc}"', mafic.SearchType.YOUTUBE_MUSIC)
 			else:
 				_search_results = await self.pl.fetch_tracks(f"{track.author} - {track.title}", mafic.SearchType.YOUTUBE)
 			if _search_results and isinstance(_search_results, list) and len(_search_results) > 0:
